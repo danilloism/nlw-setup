5b4f8ed4ccafc8d01400e8c0cb5e3c5005f35cb0
Danillo Ilggner <danilloilggner@gmail.com>
2023-01-17 09:25:13 -0300

    initial backend setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,17 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return 'alo';
+app.register(cors);
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
 });
 
-app.listen({ port: 3333 });
+app.listen({ port: 3333 }, () => {
+  console.log('Servidor iniciado');
+});
